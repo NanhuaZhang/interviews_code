97ed24076fd962dd23339835211aad032b4c774f
naylorzhang <naylorzhang@tencent.com>
2022-06-17 22:22:19 +0800

    feat: 正则匹配,使用动态规划

--- week6/isMatch.js
+++ week6/isMatch.js
@@ -4,92 +4,44 @@
  * @return {boolean}
  */
 var isMatch = function(s, p) {
-    let duplicateStr = '';
+    let sL = s.length;
+    let pL = p.length;
 
-    let randomState = false;
-    let randomStateNextCh = '';
+    // 空串起手
+    const list = new Array(sL+1).fill(false).map(()=>new Array(pL+1).fill(false));
+    list[0][0] = true;
 
-    let j = 0;
-
-    for(let i=0;i<s.length;i++){
-        if(randomState){
-            // 直接匹配.*
-            if(!randomStateNextCh){
-                return true;
-            }else{
-                // 匹配.*字母
-                if(s[i] === randomStateNextCh){
-                    randomState = false;
-                    randomStateNextCh = '';
-                    j++;
-                }else{
-                    continue;
+    for(let i = 0; i <= sL ;i++){
+        for(let j = 1;j <= pL ;j++){
+            // 第j个元素
+            if(p.charAt(j-1) === '*'){
+                list[i][j] = list[i][j-2];
+                if(matches(s,p,i,j-1)){
+                    list[i][j] =  list[i-1][j] || list[i][j-2];
                 }
-            }
-        }
-
-        if(duplicateStr){
-            if(s[i] === duplicateStr){
-                if(p[j] === duplicateStr){
-                    j++;
-                }
-                continue;
             }else{
-                duplicateStr = '';
-            }
-        }
-
-        if(j === p.length){
-            return false;
-        }
-
-        if(p[j] === '.'){
-            // .*
-            if(j+1 < p.length && p[j+1] === '*'){
-                randomState = true;
-                if(j+2< p.length){
-                    randomStateNextCh = p[j+2];
+                if(matches(s,p,i,j)){
+                    list[i][j] = list[i-1][j-1];
                 }
-                j+=2;
-            }else{
-                j++;
-            }
-            continue;
-        }
-
-        // 字母*
-        if(j+1 < p.length && p[j+1] === '*'){
-            duplicateStr = p[j];
-        }
-
-        if(s[i] === p[j]){
-            j += 1;
-            if(duplicateStr){
-                j+=1;
-            }
-        }else{
-            // 字母*匹配0个
-            if(duplicateStr){
-                duplicateStr = '';
-                j+=2;
-                i-=1;
-            }else {
-                return false;
             }
         }
     }
 
-    if(j < p.length){
-        return false;
-    }
-
-    if(randomState){
-        return false;
-    }
-
-    return true;
+    return list[sL][pL];
 };
 
-// console.log(isMatch('aa','a*'))
-console.log(isMatch('aaa','ab*a*c*a'))
-// console.log(isMatch('aaa','ab*a'))
+function matches(s,p,i,j){
+    if(i === 0){
+        return false;
+    }
+
+    if(s.charAt(i-1) === p.charAt(j-1)){
+        return true;
+    }
+
+    if(p.charAt(j-1) === '.'){
+        return true;
+    }
+
+    return false;
+}
